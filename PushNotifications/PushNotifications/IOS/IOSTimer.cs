184db46844830f30283616a07c18481dbef46ce8
Eduardo Oviedo Blanco <eduardo.oviedo@gmail.com>
2013-02-04 16:56:33 -0600

    timer

---
+++ PushNotifications/PushNotifications/IOS/IOSTimer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PushNotifications.IOS
+{
+    class IOSTimer
+    {
+        private static System.Timers.Timer mainTimer;
+
+        public static void Start()
+        {
+            mainTimer = new System.Timers.Timer(1);
+            mainTimer.Elapsed += new System.Timers.ElapsedEventHandler(OnTimeEvent);
+            mainTimer.Interval =20000;
+            mainTimer.Enabled = true;            
+        }
+
+        private static void OnTimeEvent(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            Console.WriteLine("Event received at {0}", e.SignalTime);
+        }
+    }
+}
---
+++ PushNotifications/PushNotifications/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PushNotifications.IOS;
+
+namespace PushNotifications
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            IOSTimer.Start();
+
+            Console.WriteLine("Press enter to stop service.");
+            Console.ReadLine();
+        }
+    }
+}
