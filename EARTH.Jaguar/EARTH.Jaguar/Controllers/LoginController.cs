9b574cf36570be59a47feef74cefc97ce5f8172c
Eduardo Oviedo Blanco <eduardo.oviedo@gmail.com>
2013-05-08 16:56:51 -0600

    .

--- EARTH.Jaguar/EARTH.Jaguar/Controllers/LoginController.cs
+++ EARTH.Jaguar/EARTH.Jaguar/Controllers/LoginController.cs
@@ -17,7 +17,6 @@ namespace EARTH.Jaguar.Controllers
     {
         //POST api/login
         [AllowAnonymous]
-        
         public HttpResponseMessage Post(LogOnModel model)
         {
             try 
